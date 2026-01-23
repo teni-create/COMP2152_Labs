fd903358b4d10c47c2aa4ec876bdd3da4d233c3a
teni-create <teni.ogun@georgebrown.ca>
2026-01-23 09:45:47 -0500

    Complete Week 02 Solution

--- Week02/Lab02.py
+++ Week02/Lab02.py
@@ -1,20 +1,21 @@
 import random
 
-choices = ["Rock","Paper","Scissors"]
+choices = ["Rock", "Paper", "Scissors"]
 
-playerChoice = input("Enter your choice (1-Rock, 2-Paper, 3-Scissors): ")
+playerChoice = input("Enter your choice (1-Rock, 2-Paper, 3-Scissors): ") # 1, 2, 3
 
-playerChoice=int(playerChoice)
-
-print(playerChoice)
+playerChoice = int(playerChoice)
 
 if playerChoice < 1 or playerChoice > 3:
-    print("Error: The input should be an integer between 1 and 3.")
+    print("Error: Choice should be between 1 and 3!")
 else:
-    # Determine the winner logic sing if/elif/else
-    computerChoice = random.randint(1,3)
+    computerChoice = random.randint(1, 3)
 
+    playerChoiceIndex = choices[playerChoice - 1] # ==> 0, 1, 2
+    computerChoiceIndex = choices[computerChoice - 1]
+    print(playerChoiceIndex,  computerChoiceIndex)
 
+    # Determine the winner logic using if/elif/else
     if playerChoice == computerChoice:
         print("It's a tie!")
     elif playerChoice == 1 and computerChoice == 3:
